e5fede9cbf534a1efd76bad5173203538352f761
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2026-06-10 17:25:23 -0300

    feat: replace font dependencies with Fira Sans and update layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-  import "@fontsource-variable/inter";
-  import "@fontsource-variable/geist";
-
+  import "@fontsource/fira-sans/300.css";
+  import "@fontsource/fira-sans/400.css";
+  import "@fontsource/fira-sans/500.css";
+  import "@fontsource/fira-sans/600.css";
+  import "@fontsource/fira-sans/700.css";
+  import "@fontsource/fira-sans/800.css";
   import "./layout.css";
+
   import favicon from "$lib/assets/favicon.svg";
 
   let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
 {@render children()}
