5bd08edde085b3f32e4172499e85f9db425140f0
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2026-06-10 18:11:55 -0300

    refactor: update ESLint configuration, clean up package.json, and remove unused index.ts file

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,40 @@
-import prettier from "eslint-config-prettier";
 import path from "node:path";
-import js from "@eslint/js";
-import svelte from "eslint-plugin-svelte";
 import { defineConfig, includeIgnoreFile } from "eslint/config";
-import globals from "globals";
+import js from "@eslint/js";
 import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
 import svelteConfig from "./svelte.config.js";
 
-const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
-
 export default defineConfig(
-  includeIgnoreFile(gitignorePath),
+  includeIgnoreFile(path.resolve(import.meta.dirname, ".gitignore")),
   js.configs.recommended,
   ts.configs.recommended,
   svelte.configs.recommended,
   prettier,
   svelte.configs.prettier,
   {
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     rules: {
-      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      // https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
       "no-undef": "off",
     },
   },
   {
-    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    files: ["**/*.svelte", "**/*.svelte.ts"],
     languageOptions: {
       parserOptions: {
+        parser: ts.parser,
         projectService: true,
         extraFileExtensions: [".svelte"],
-        parser: ts.parser,
         svelteConfig,
       },
     },
-  },
-  {
-    // Override or add rule settings here, such as:
-    // 'svelte/button-has-type': 'error'
-    rules: {},
   },
 );
